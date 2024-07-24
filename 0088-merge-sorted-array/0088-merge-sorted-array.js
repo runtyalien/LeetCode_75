66accd759672e937781a091b3becbd5bfd04ad18
runtyalien <omgadkar@gmail.com>
2024-07-24 22:03:22 +0530

    Time: 60 ms (22.52%), Space: 48.5 MB (92.85%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,16 +6,18 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let i = 0;
+    let p1 = m - 1;
+    let p2 = n - 1;
+    let p = m + n - 1;
     
-    if(n === 0) return nums1;
-    
-    while( i < nums2.length){
-        nums1[i+m] = nums2[i] ;
-        i++;
+    while (p2 >= 0) {
+        if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+            nums1[p] = nums1[p1];
+            p1--;
+        } else {
+            nums1[p] = nums2[p2];
+            p2--;
+        }
+        p--;
     }
-    
-    nums1.sort((a,b) => a-b);
-    
-    return nums1;
 };
