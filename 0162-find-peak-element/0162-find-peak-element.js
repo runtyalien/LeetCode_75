7c2ec63479d653cc73e99a500518f8e81a5b93fc
runtyalien <omgadkar@gmail.com>
2024-07-27 19:59:59 +0530

    Time: 45 ms (88.07%), Space: 48.9 MB (47.79%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -3,17 +3,18 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    let index;
-    if(nums.length === 1){
-        index = 0;
-    } else {    
-    for(let i = 0; i < nums.length; i++){
-        if((i === 0 && nums[i] > nums[i+1]) || (i === nums.length-1 && nums[i-1] < nums[i])){
-            index = i;
-        } else if(nums[i] > nums[i-1] && nums[i] > nums[i+1]){
-            index = i;
+        let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] > nums[mid + 1]) {
+            right = mid;
+        } else {
+            left = mid + 1;
         }
     }
-    }
-    return index;
+
+    return left;
 };
