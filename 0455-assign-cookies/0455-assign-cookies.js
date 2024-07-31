a12c3c7611980ab23fc489b23f6e14105894d68b
runtyalien <omgadkar@gmail.com>
2024-07-31 23:10:54 +0530

    Time: 69 ms (89.79%), Space: 51.9 MB (59.15%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    g.sort((a,b) => a-b);
+    s.sort((a,b) => a-b);
+    
+    let child = 0;
+    let cookie = 0;
+    let count = 0;
+    
+    while(child < g.length && cookie < s.length){
+        if(s[cookie] >= g[child]){
+            count++;
+            child++;
+        }
+        cookie++;
+    }
+    
+    return count;
+};
