ffe39cac5ea63542eb9a76011dc019d0a9b1e2d7
runtyalien <omgadkar@gmail.com>
2024-08-03 01:21:22 +0530

    Time: 50 ms (89.93%), Space: 51.8 MB (12.05%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -4,26 +4,19 @@
  * @return {number[]}
  */
 var intersect = function(nums1, nums2) {
-    nums1.sort((a,b) => a - b);
-    nums2.sort((a,b) => a - b);  
-
-    // let minLen = Math.min(nums1.length, nums2.length);
-    let arr = [];
-    let count = 0;
-    let num1 = 0;
-    let num2 = 0;
+    let map = new Map();
+    let result = [];
     
-    while(num1 < nums1.length && num2 < nums2.length){
-        if(nums1[num1]  === nums2[num2]){
-            arr.push(nums1[num1]);
-            num1++;
-            num2++;
-         } else if(nums1[num1] < nums2[num2]){
-             num1++;
-         } else {
-             num2++;
-         }
+    for(let num of nums1){
+        map.set(num, (map.get(num) || 0) + 1);
     }
     
-    return arr;
+    for(let num of nums2){
+        if(map.get(num) > 0 ){
+            result.push(num);
+            map.set(num, map.get(num) - 1);
+        }
+    }
+    
+    return result;
 };
